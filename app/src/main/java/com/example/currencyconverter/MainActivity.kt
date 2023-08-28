b7ee5b43cf21e570e6228d6a382aadd6dfddf870
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2023-08-28 18:56:43 +0500

    Первый коммит

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.currencyconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.currencyconverter.ui.theme.CurrencyConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CurrencyConverterTheme {
+        Greeting("Android")
+    }
+}
